86962579e5e75697e056341741d3d5f5a5ebf3fb
Rahul Chaudhary <rahulchaudhary301@gmail.com>
2023-05-24 02:37:23 +0530

    done

--- src/SQL.js
+++ src/SQL.js
@@ -6,7 +6,7 @@ const sql=require('mysql')
 const connection=sql.createConnection({
     host:"bb09tmo9ia7rkvjlgitr-mysql.services.clever-cloud.com",
     user:"umd4rw6itdnna500",
-    password:' ',
+    password:'6yxOZUboNV7Y00AWbf7x',
     database:"bb09tmo9ia7rkvjlgitr"
 });
 
